7fada7c5b72b5c608dc076729e7b5c136ebb544b
Marcos García <garciafmarcos@gmail.com>
2022-04-28 00:23:36 +0200

    Initial

---
+++ init.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', () => {
+  const content = document.querySelector('#content');
+  const table = document.querySelector('#table');
+  const thead = document.querySelector('#thead');
+  const label = document.querySelector('#label');
+  const tablePos = table.offsetTop;
+  const contentPos = content.offsetTop;
+
+  let pageScroll = window.pageYOffset;
+  let i = pageScroll - contentPos;
+
+  if (pageScroll >= contentPos) {
+    thead.style.cssText = `transform: translateY(${i}px);`;
+    label.classList.add('label--in');
+    content.style.cssText= 'padding-top: 0;'
+  } else {
+    i = 0;
+    thead.style.cssText = `transform: translateY(0px);`;
+    label.classList.remove('label--in');
+    content.style.cssText= 'padding-top: 50px;'
+  }
+});
